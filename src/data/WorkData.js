8ce48ff86a2de221eda02f2352581a86add495c0
Rongon <rongonhasan2@.com>
2023-11-08 01:51:10 +0600

    first commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Resturent BB",
+        description:"Resturent  is a full stack wepsite provided by Rongon. It is a one-stop solution for all your foods needs. You can find every food need about your destination, including  many kind of food items. it is full stack beutifull project",
+        tags:["react","node","mongo", "Firebase", "stripe"],
+        
+        demo:"https://bistro-boss-1cebd.web.app/",
+        github:"https://github.com/Rongonhasan/resturent-client"
+    },
+    {
+        id:2,
+        name:"Guide-23",
+        description:"Guide 23 is a Travel Guide service provided by Rongon. It is a one-stop solution for all your travel needs. You can find all the information you need about your destination, including hotels, restaurants, attractions, and more",
+        tags:["react","node", "mongo", "firebase"],
+        
+        demo:"https://guide-22.web.app/",
+        github:"https://github.com/Rongonhasan/Guide-client"
+        
+    },
+    {
+        id:3,
+        name:"IMdb-Site",
+        description:"This React project will provide us the list of upcoming, popular & top rated movies. On clicking on any movie it will show us the detail page of the movie.This React app also have a caraousel at the top for displaying movies as hero banner.",
+        tags:["react","api","sass"],
+        
+        demo:"https://imdb-site-rongon.netlify.app/'",
+        github:"https://github.com/Rongonhasan/IMDb-site",
+    },
+    {
+        id:4,
+        name:"Gericht-resturent",
+        description:" The Key To Fine Dining.Good quaility resturent wep page use react js and row css , thank you",
+        tags:["react","row css","api"],
+        
+        demo:"https://gerich-resturent.vercel.app/",
+        github:"https://github.com/Rongonhasan/gerich-resturent"
+       
+    },{
+        id:5,
+        name:"color-palette-generator",
+        description:"A Color Palette Generator From Images, it help us to find image color. next js wepsite",
+        tags:["Next Js",],
+        
+        demo:"https://color-palette-generator-gray.vercel.app/",
+        github:"https://github.com/Rongonhasan/color-palette-generator"
+        
+    },
+    {
+        id:6,
+        name:"Meal Map",
+        description:"Meal Map is a website to search food recipes around the world, this website uses the edamam API.",
+        tags:["Javascript","sass","API"],
+        
+        demo:"https://mealmap-rongon.vercel.app/",
+        github:"https://github.com/Rongonhasan/mealmap-rongon"
+        
+    },
+    
+  
+] 
