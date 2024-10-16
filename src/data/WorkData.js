49f3786e3f93aa9920c3603625769ed0d9a5d4b7
Rongon <rongonhasan2@.com>
2024-10-17 01:41:41 +0600

    done

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,34 +3,55 @@
 export const Work = [
     {
         id:1,
-        name:"Resturent BB",
+        name:"Helium-Dine",
         description:"Resturent  is a full stack wepsite provided by Rongon. It is a one-stop solution for all your foods needs. You can find every food need about your destination, including  many kind of food items. it is full stack beutifull project",
         tags:["react","node","mongo", "Firebase", "stripe"],
         
         demo:"https://bistro-boss-1cebd.web.app/",
         github:"https://github.com/Rongonhasan/resturent-client"
+      
     },
     {
         id:2,
-        name:"Guide-23",
-        description:"Guide 23 is a Travel Guide service provided by Rongon. It is a one-stop solution for all your travel needs. You can find all the information you need about your destination, including hotels, restaurants, attractions, and more",
-        tags:["react","node", "mongo", "firebase"],
+        name:"Youtube",
+        description:"React.js, Material UI, and just a couple of other dependencies, you'll build everything else from scratch!",
+        tags:["react","axios","react-player","Material Ui",],
         
-        demo:"https://guide-22.web.app/",
-        github:"https://github.com/Rongonhasan/Guide-client"
+        demo:"https://youtube-clone-rongon.vercel.app/",
+        github:"https://github.com/Rongonhasan/youtube-clone_rongon"
         
     },
     {
         id:3,
-        name:"IMdb-Site",
+        name:"Movie-X",
         description:"This React project will provide us the list of upcoming, popular & top rated movies. On clicking on any movie it will show us the detail page of the movie.This React app also have a caraousel at the top for displaying movies as hero banner.",
         tags:["react","api","sass"],
         
-        demo:"https://imdb-site-rongon.netlify.app/'",
-        github:"https://github.com/Rongonhasan/IMDb-site",
+        demo:"https://movie-x-pearl.vercel.app/",
+        github:"https://github.com/Rongonhasan/MovieX",
     },
     {
         id:4,
+        name:"Guide-23",
+        description:"Guide 23 is a Travel Guide service provided by Rongon. It is a one-stop solution for all your travel needs. You can find all the information you need about your destination, including hotels, restaurants, attractions, and more",
+        tags:["react","node", "mongo", "firebase"],
+        
+        demo:"https://guide-client-yadd.vercel.app",
+        github:"https://github.com/Rongonhasan/Guide-client"
+        
+    },
+    {
+        id:5,
+        name:"Forever-Ecommerce",
+        description:"Modern Design & User Authentication Developed with React and Tailwind CSS for a responsive, including secure login and signup functionality Product Discovery Features advanced search, filtering options, and comprehensive detail pages ",
+        tags:["React, tailwind",],
+        
+        demo:"https://forever-pi.vercel.app/",
+        github:"https://github.com/Rongonhasan/forever"
+        
+    },
+    {
+        id:6,
         name:"Gericht-resturent",
         description:" The Key To Fine Dining.Good quaility resturent wep page use react js and row css , thank you",
         tags:["react","row css","api"],
@@ -38,26 +59,8 @@ export const Work = [
         demo:"https://gerich-resturent.vercel.app/",
         github:"https://github.com/Rongonhasan/gerich-resturent"
        
-    },{
-        id:5,
-        name:"color-palette-generator",
-        description:"A Color Palette Generator From Images, it help us to find image color. next js wepsite",
-        tags:["Next Js",],
-        
-        demo:"https://color-palette-generator-gray.vercel.app/",
-        github:"https://github.com/Rongonhasan/color-palette-generator"
-        
     },
-    {
-        id:6,
-        name:"Meal Map",
-        description:"Meal Map is a website to search food recipes around the world, this website uses the edamam API.",
-        tags:["Javascript","sass","API"],
-        
-        demo:"https://mealmap-rongon.vercel.app/",
-        github:"https://github.com/Rongonhasan/mealmap-rongon"
-        
-    },
+  
     
   
 ] 
